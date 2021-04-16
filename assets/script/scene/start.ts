580184a4e047f420a92bf51c080d33924a4448f8
muzzik <1226085293@qq.com>
2021-04-17 06:13:28 +0800

    # 修改随机速度

--- assets/script/scene/start.ts
+++ assets/script/scene/start.ts
@@ -25,7 +25,14 @@ export default class start extends cc.Component {
             return;
         }
         this._rolling_lottery_os.forEach(v1_o=> {
-            v1_o.scroll(Math.floor(Math.random() * v1_o.node.children.length));
+            let old_speed_n = v1_o.max_speed_n;
+            // 修改速度让其更加真实
+            v1_o.max_speed_n += Math.floor(Math.random() * 11) - 5;
+            v1_o.scroll(Math.floor(Math.random() * v1_o.node.children.length), {
+                "rebound_finish_cb_f": ()=> {
+                    v1_o.max_speed_n = old_speed_n;
+                }
+            });
         });
     }
     public btn_必中抽奖(): void {
@@ -36,7 +43,14 @@ export default class start extends cc.Component {
         let index_n = Math.floor(Math.random() * this._rolling_lottery_os[0].node.children.length);
         let target_s = this._rolling_lottery_os[0].node.children[index_n].name;
         this._rolling_lottery_os.forEach(v1_o=> {
-            v1_o.scroll(v1_o.node.children.findIndex(v2_o=> v2_o.name === target_s));
+            let old_speed_n = v1_o.max_speed_n;
+            // 修改速度让其更加真实
+            v1_o.max_speed_n += Math.floor(Math.random() * 11) - 5;
+            v1_o.scroll(v1_o.node.children.findIndex(v2_o=> v2_o.name === target_s), {
+                "rebound_finish_cb_f": ()=> {
+                    v1_o.max_speed_n = old_speed_n;
+                }
+            });
         });
     }
 }
